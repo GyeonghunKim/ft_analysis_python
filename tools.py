a1a99228aa360bb1c258d4ed3d64189bbf758619
GHKim <aass98998@snu.ac.kr>
2018-09-21 21:37:13 +0900

    add function find_peak_from_kmft(); It find peak from image and you can choose output whether 512*512 bool image or peak data

--- tools.py
+++ tools.py
@@ -11,14 +11,35 @@ import peak as pk
 import movie_io as fr
 import trace as tr
 
-def make_stakced_peak(dire, movie_name):
+from scipy.ndimage import maximum_filter
+
+def init_param():
     param = par.parameter()
     param.add_attr('background', 0)
     param.add_attr('sub_rad', 4)
+    return param
+
+def make_stakced_peak(dire, movie_name, param):
     movie = fr.frame(dire, movie_name, param)
-    for i in range(frame.length):
+    for i in range(movie.length):
         one_frame = movie.get_next_frame()
         
-
-
-
+def find_peak_from_kmft(image, param, background = None, maxfiltersize = 3, thre_rat = 0.4, output_type = 'image'):
+    image = float(image)
+    if background is None:
+        background = param.get_attr('background')
+    nbimage = (image - background) # image_wobg = image without background; background can be scalar or 512*512 np array
+    nbimage = (1 - (nbimage)) * nbimage / 2
+    max_filter = maximum_filter(nbimage, size = maxfiltersize)
+    ispeak = (nbimage == max_filter)
+    xsize = int(self.param.get_attr('movie_x_size'))
+    ysize = int(self.param.get_attr('movie_y_size'))
+    if output_type == 'image':
+        return ispeak
+    else 
+        data = np.array([])
+        for i in range(self.xsize):
+            for j in range(self.ysize):
+                if ispeak[i, j] and (image[i, j] > thre_rat * image.max()):
+                    data = np.append(data, [i, j], axis = 0)
+        return data
